5f85b2cf3724a65694f2e2ad7833c069eb02ca1d
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-10-19 17:17:12 +0200

    Add project files.

---
+++ Inlämningsuppgift 4.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Uppgift_4._2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool avsluta = false;
+        int störstHeltal = 0;
+        while (avsluta != true)
+        {
+            Console.WriteLine("Vill du skriva ett heltal? Om inte, skriv 'n', annars skriv ditt heltal");
+            string svar = Console.ReadLine();
+            try
+            {
+                if (svar == "n")
+                {
+                    avsluta = true;
+                }
+                else if (störstHeltal < int.Parse(svar))
+                {
+                    störstHeltal = int.Parse(svar);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Du kan endast skriva ett heltal eller 'n'");
+            }
+        }
+        Console.WriteLine("Det störta talet du skrev in var "+störstHeltal);
+        Console.Read();
+    }
+}
